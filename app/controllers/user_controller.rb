9fbc47a0b69db0102f4a325a5ddd40f102d3c39e
Anuswan91 <vivien.blum@free.com>
2017-09-06 16:00:15 +0200

    Action setup

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,10 +1,13 @@
 class UserController < ApplicationController
   def index
+    puts "ACTION INDEX"
   end
 
-  def index
+  def show
+    puts "ACTION SHOW"
   end
 
   def create
+    puts "ACTION CREATE"
   end
 end
