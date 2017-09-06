b2e715d66292c9edc248df09e030489cc4dbd2b5
Anuswan91 <vivien.blum@free.com>
2017-09-06 16:07:22 +0200

    model & migration setup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170906134048) do
+ActiveRecord::Schema.define(version: 20170906140650) do
+
+  create_table "movies", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "runtime"
+    t.float    "rate"
+    t.string   "image"
+    t.text     "plot"
+    t.string   "poster"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
