7209ba29597ef99b51e7896b7f27f4ccf33169b5
Anuswan91 <vivien.blum@free.com>
2017-09-06 15:43:38 +0200

    Action show, index, create & model User

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,10 @@
+class UserController < ApplicationController
+  def index
+  end
+
+  def index
+  end
+
+  def create
+  end
+end
---
+++ db/migrate/20170906134048_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :mail
+      t.string :password
+      t.string :image
+
+      t.timestamps null: false
+    end
+  end
+end
