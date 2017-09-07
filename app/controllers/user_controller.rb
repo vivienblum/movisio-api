a09cb818a2721b59cb26397c79f2de1e13b08724
Anuswan91 <vivien.blum@free.com>
2017-09-07 11:16:30 +0200

    route show

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,14 +1,22 @@
 class UserController < ApplicationController
   def index
     puts "ACTION INDEX"
+    # TODO don't send password
     render json: {status: 'OK', users: User.all }
   end
 
   def show
     puts "ACTION SHOW"
+    render json: {status: 'OK', user: User.find(params[:id]) }
   end
 
   def create
     puts "ACTION CREATE"
+  end
+
+  private
+
+  def users_params
+    params.require(:user).permit!
   end
 end
