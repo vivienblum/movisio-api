081c53ac646c75f9c39a730bf001436051882036
Anuswan91 <vivien.blum@free.com>
2017-09-07 10:42:26 +0200

    set api default

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,8 @@ module MovisioApi
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    config.api_only = true
+    config.autoload_paths << Rails.root.join('lib')
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
@@ -30,6 +31,9 @@ module MovisioApi
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    # config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
+    #   rewrite %r{/movisio_api(-v.)*(.*)}, '$2'
+    # end
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
