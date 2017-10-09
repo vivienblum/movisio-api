037f4976135624111ec7766bf7c30c24af7df233
Anuswan91 <vivien.blum@free.fr>
2017-10-09 13:19:04 -0700

    Add movie to user works

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -46,6 +46,7 @@ class UserController < ApplicationController
 
   def get_movies
     # render json: {status: 'OK', movies: current_user.users_movies.eager_load(:movies) }
+    # TODO get from params[:id]
     movies = []
     current_user.users_movies.each do |user_movie|
       movie = Movie.find(user_movie.movie_id).attributes.symbolize_keys
@@ -58,7 +59,12 @@ class UserController < ApplicationController
   end
 
   def add_movie
-    render json: {status: 'OK', movie: UsersMovie.create(user_id: current_user.id, movie_id: params[:movie_id]) }
+    # TODO disable duplicate
+    user_movie = UsersMovie.create(user_id: current_user.id, movie_id: params[:movie_id])
+    movie = Movie.find(user_movie.movie_id).attributes.symbolize_keys
+    movie[:watched] = user_movie.watched
+    movie[:favorite] = user_movie.favorite
+    render json: {status: 'OK', movie: movie }
   end
 
   def toggle_watched
