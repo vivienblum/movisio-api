5ea3d20a69587f7e0546be76f500f4ae1e008a89
Anuswan91 <vivien.blum@free.fr>
2017-10-09 12:27:31 -0700

    Added year to movies

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171009191453) do
+ActiveRecord::Schema.define(version: 20171009192402) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20171009191453) do
     t.string   "poster"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "year"
   end
 
   create_table "users", force: :cascade do |t|
