044ac81a26985d21d056fea3b54a859586198f8e
Anuswan91 <vivien.blum@free.fr>
2017-10-26 12:38:22 -0700

    Remove status OK

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -1,15 +1,15 @@
 class MovieController < ApplicationController
 
   def index
-    render json: {status: 'OK', movies: Movie.all }
+    render json: { movies: Movie.all }
   end
 
   def show
-    render json: {status: 'OK', movie: Movie.find(params[:id]) }
+    render json: { movie: Movie.find(params[:id]) }
   end
 
   def create
-    render json: {status: 'OK', movie: Movie.create(movies_params) }
+    render json: { movie: Movie.create(movies_params) }
   end
 
   private
